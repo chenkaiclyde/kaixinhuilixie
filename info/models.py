31aa1ea48596e7e896147b69717424677dc313a6
Clyde <chenkaiclyde@163.com>
2019-06-18 18:31:59 -0700

    订单表

--- info/models.py
+++ info/models.py
@@ -36,14 +36,28 @@ class ShippingAddress(BaseModel, db.Model):
 
     user = db.relationship('User', backref='shipping_addr', lazy='dynamic')
 
+
 class Product(BaseModel, db.Model):
     """用户"""
     __tablename__ = "product"
-    id = db.Column(db.Integer, primary_key=True,nullable=False)  # 收件地址id
+    id = db.Column(db.Integer, primary_key=True, nullable=False)  # 收件地址id
     title = db.Column(db.String(255), nullable=False)  # 商品标题
     price = db.Column(db.INTEGER, nullable=False)  # 商品价格
     describe = db.Column(db.String(255), nullable=True)  # 商品描述
     category_id = db.Column(db.INTEGER, nullable=False)  # 商品种类
+    all_nums = db.Column(db.INTEGER, nullable=False, default=0)  # 商品数量
+    status = db.Column(db.INTEGER, nullable=False, default=2)  # 商品状态:0上架中，1下架，2审核中
+    brand_id = db.Column(db.INTEGER, nullable=False)  # 品牌id
+    seller_id = db.Column(db.INTEGER, nullable=False)  # 卖家id
+    grade = db.Column(db.INTEGER, nullable=False, default=0)  # 商品评分，默认是0
 
-    user = db.relationship('User', backref='shipping_addr', lazy='dynamic')
 
+class OrderForm(BaseModel, db.Model):
+    """用户"""
+    __tablename__ = "order_form"
+
+    id = db.Column(db.Integer, primary_key=True)  # 订单id
+    user_id = db.Column(db.Integer, nullable=False, primary_key=True)  # 用户id
+    address_id = db.Column(db.INTEGER, nullable=False, primary_key=True)  # 地址id
+    status = db.Column(db.INTEGER, nullable=False, default=0)  # 定单状态，0代付款，1待发货，2待收货，3已签收，4已取消
+    payment_method = db.Column(db.INTEGER, nullable=False)  # 付款方式，0支付宝，1银联，2ebay，3微信
